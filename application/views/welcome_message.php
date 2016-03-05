6318af787c38e696f2a7f905c5b4cb3f8f6808fa
Kevin Raneri <kevin.raneri@gmail.com>
2016-03-05 13:41:02 -0500

    Revert "Edited welcome message"
    This reverts commit 257b5a2f155d358e14188928efcded85d1f399ce.

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -68,9 +68,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <body>
 
 <div id="container">
-	<h1>Welcome to IguPlanning!</h1>
+	<h1>Welcome to CodeIgniter!</h1>
 
 	<div id="body">
+<<<<<<< HEAD
 		<p>You will need to log in to view this page!</p>
 		
 		<form action='/application/views/login.php' method=POST>
@@ -78,6 +79,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			Password: <input type=password name='password'/><br/>
 			<input type=submit value='Login'/>
 		</form>
+=======
+		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+
+		<p>If you would like to edit this page you'll find it located at:</p>
+		<code>application/views/welcome_message.php</code>
+
+		<p>The corresponding controller for this page is found at:</p>
+		<code>application/controllers/Welcome.php</code>
+
+		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+>>>>>>> parent of 257b5a2... Edited welcome message
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
